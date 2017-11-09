6cc95620a78fc9d788fe12ebf3e9cce0be0c2152
oncefloating <zhaodandaneffy@163.com>
2017-11-09 19:25:30 +0800

    v0.5 with form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from app import app
 from flask import render_template
 from models import Todo
+from flask import request
 
 @app.route('/')
 def index():
+    todos = Todo.objects.all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add',methods=['POST',])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content=content)
+    todo.save()
     todos = Todo.objects.all()
     return render_template('index.html', todos=todos)
